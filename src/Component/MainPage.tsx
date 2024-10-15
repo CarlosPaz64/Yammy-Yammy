ffd812e111a888f029edd262b3ae829a6d90a702
JoseCob <josecobkantun123@gmail.com>
2024-10-15 13:10:45 -0600

    Se esta planificando la estructura del proyecto
    Actualización de la estructura del proyecto, aun esta en fase de desarrollo el diseño

---
+++ src/Component/MainPage.tsx
@@ -0,0 +1,20 @@
+//Aqui se agruparan todos los componentes de la página principal
+function MainPage(){
+    return (
+        <>
+            {/*Navbar*/}
+            {/*Fin del Navbar*/}
+
+            {/*Carrusel de productos*/}
+            <div>
+                Hola Mundo :3
+            </div>
+            <h2>Bienvenido a REACT</h2>
+            {/*Fin del carrusel productos*/}
+
+            {/*Footer*/}
+            {/*Fin del Footer*/}
+        </>
+    )
+}
+export default MainPage
