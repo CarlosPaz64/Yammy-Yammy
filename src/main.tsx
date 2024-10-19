16f04a45977c02519b8b6942e2bc7f7589eaffc9
moma14 <151698538+moma14@users.noreply.github.com>
2024-10-19 01:44:34 -0600

    creacion de formulario con validaciones zod para el apartado Haz un pedido

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import MainPage from './pagina_principal/MainPage.tsx';
+import App from './App';
+//import MainPage from './pagina_principal/MainPage.tsx';
 import './css/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <MainPage />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
