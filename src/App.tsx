e5bfcb4383137051463a36f95e888643876ecbcd
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-10-19 02:16:17 -0600

    Añadido de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-//import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 //import MainPage from './pagina_principal/MainPage';
-//import MenuPage from './pages/MenuPage';
+import MenuPage from './menu/menu';
 //import PedidoPage from './pages/PedidoPage';
 //import ConocenosPage from './pages/ConocenosPage';
 //import PerfilPage from './pages/PerfilPage';
 //import CarritoPage from './pages/CarritoPage';
 import NavBar from './pagina_principal/NavBar/NavBar'; // Asegúrate de que NavBar esté en la ruta correcta
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
@@ -14,16 +14,15 @@ function App() {
       {/* El NavBar se muestra en todas las páginas */}
       <NavBar />
       
-      {/* 
       <Routes>
         {/* Define tus rutas aquí */}
         {/* <Route path="/" element={<MainPage />} /> */}
-        {/* <Route path="/menu" element={<MenuPage />} /> */}
+        <Route path="/menu" element={<MenuPage />} /> 
         {/* <Route path="/pedido" element={<PedidoPage />} /> */}
         {/* <Route path="/conocenos" element={<ConocenosPage />} /> */}
         {/* <Route path="/perfil" element={<PerfilPage />} /> */}
         {/* <Route path="/carrito" element={<CarritoPage />} /> */}
-      {/*</Routes>*/}
+      </Routes>
     </>
   );
 }
