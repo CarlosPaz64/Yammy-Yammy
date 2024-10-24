2a7886d668836e6441a1a7162da337508fd1fe4b
moma14 <151698538+moma14@users.noreply.github.com>
2024-10-23 22:54:25 -0600

    correcion de errores en la merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 //import MainPage from './pagina_principal/MainPage';
 //import MenuPage from './pages/MenuPage';
- import Pedido from './pagina_principal/HazUnpedido/Pedidos';
+import Pedido from './pagina_principal/HazUnpedido/Pedidos';
 //import ConocenosPage from './pages/ConocenosPage';
 //import PerfilPage from './pages/PerfilPage';
 //import CarritoPage from './pages/CarritoPage';
@@ -13,18 +13,17 @@ function App() {
   return (
     <>
       <NavBar />
-      
 
       <Routes>
         {/* Define tus rutas aquí */}
-         {/*<Route path="/" element={<MainPage />} /> */}
+        {/*<Route path="/" element={<MainPage />} /> */}
         {/* <Route path="/menu" element={<MenuPage />} /> */}
         <Route path="/pedido" element={<Pedido />} /> 
         {/* <Route path="/conocenos" element={<ConocenosPage />} /> */}
         {/* <Route path="/perfil" element={<PerfilPage />} /> */}
         {/* <Route path="/carrito" element={<CarritoPage />} /> */}
 
-      <Routes>
+        {/* Mueve el bloque de Routes cerrado correctamente */}
         <Route path="/registro" element={<RegisterForm />} />
       </Routes>
     </>
