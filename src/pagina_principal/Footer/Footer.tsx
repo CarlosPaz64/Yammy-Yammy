7666fe261f330284dfc6e8c7e5c8fd3799624e81
JoseCob <josecobkantun123@gmail.com>
2024-10-26 23:02:47 -0600

    Cambios en el footer
    Se esta trabajando en el diseño del Footer de la página

--- src/pagina_principal/Footer/Footer.tsx
+++ src/pagina_principal/Footer/Footer.tsx
@@ -7,8 +7,23 @@ const Footer: React.FC<{ children?: ReactNode }> = ({ children }) => {
         <>
             {/*Pie de la página*/}
             <footer>
-                <h1>Hola Mundo :D</h1>
-                <p>© 2024 Mi Sitio Web. Todos los derechos reservados.</p>
+                <div className="part-coffie">
+                    <div className="spillo-chocolate1"></div>
+                    <div className="spillo-chocolate2"></div>
+                    <div className="spillo-chocolate3"></div>
+
+                </div>
+                <h1>YAMY YAMY</h1>
+                <div className="part-pink"></div>
+                <section className="section-social">
+                    {/*Aqui van las redes sociales*/}
+                    <p>-- REDES SOCIALES DE LA EMPRESA --</p>
+                </section>
+                <div className="part-pink"></div>
+                <section className="section-slogan">
+                    <h2>--SLOGAN DE LA EMPRESA--</h2>
+                </section>
+                <p>© 2024. Yamy Yamy Cake Company. Reservado todo los derechos.</p>
             </footer>
             {children}
         </>
