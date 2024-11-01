c24a7aa3cf666557af90eb92a33e117d2ed46cf9
JoseCob <josecobkantun123@gmail.com>
2024-11-01 15:33:32 -0600

    Se modifico el footer
    Actualización de diseño del footer, prototipo para la página

--- src/pagina_principal/Footer/Footer.tsx
+++ src/pagina_principal/Footer/Footer.tsx
@@ -20,22 +20,22 @@ const Footer: React.FC<{ children?: ReactNode }> = ({ children }) => {
                     <div className="melted-chocolate8"></div>
                     <div className="melted-chocolate9"></div>
                 </div>
-                <div className="container-stick">
+                <div className="container-candle">
                     <div className="word-candle">
-                        <div className="stick1-candle"></div>
-                        <div className="stick2-candle"></div>
-                        <div className="stick3-candle"></div>
-                        <div className="stick4-candle"></div>
-                        <div className="stick5-candle"></div>
-                        <div className="stick6-candle"></div>
-                        <div className="stick7-candle"></div>
-                        <div className="stick8-candle"></div>
-                        <div className="stick9-candle"></div>
-                        <div className="stick10-candle"></div>
-                        <div className="stick11-candle"></div>
-                        <div className="stick12-candle"></div>
-                        <div className="stick13-candle"></div>
-                        <div className="stick14-candle"></div>
+                        <div className="container-stick stick1"></div>
+                        <div className="container-stick stick2"></div>
+                        <div className="container-stick stick3"></div>
+                        <div className="container-stick stick4"></div>
+                        <div className="container-stick stick5"></div>
+                        <div className="container-stick stick6"></div>
+                        <div className="container-stick stick7"></div>
+                        <div className="container-stick stick8"></div>
+                        <div className="container-stick stick9"></div>
+                        <div className="container-stick stick10"></div>
+                        <div className="container-stick stick11"></div>
+                        <div className="container-stick stick12"></div>
+                        <div className="container-stick stick13"></div>
+                        <div className="container-stick stick14"></div>
                         YAMY YAMY
                         {/* Iconos de la flama en la vela */}
                         <i className="fi fi-ss-flame candle-flame flame1"></i>
@@ -56,14 +56,24 @@ const Footer: React.FC<{ children?: ReactNode }> = ({ children }) => {
                 </div>
                 <div className="part-pink"></div>
                 <section className="section-social">
-                    {/*Aqui van las redes sociales*/}
-                    <p>-- REDES SOCIALES DE LA EMPRESA --</p>
+                    {/*Redes sociales de la Repostería*/}
+                    <div className="Container-social">
+                        <h2 className="title-social">Siguenos</h2>
+                        <div className="content-social">
+                            <div className="elfsight-app-7f86dc58-f268-4aa9-801f-c10b3ee16617" data-elfsight-app-lazy></div>
+                        </div>
+                    </div>
                 </section>
                 <div className="part-pink"></div>
                 <section className="section-slogan">
-                    <h2>¡Tu antojo una realidad!</h2>
+                    {/*Slogan con Derechos reservados */}
+                    <div className="Container-slogan">
+                        <h3 className="title-slogan">¡Tu antojo una realidad!</h3>
+                        <div className="content-slogan">
+                            <p>© 2024. Yamy Yamy Cake Company. Reservado todo los derechos.</p>
+                        </div>
+                    </div>
                 </section>
-                <p>© 2024. Yamy Yamy Cake Company. Reservado todo los derechos.</p>
             </footer>
             {children}
         </>
