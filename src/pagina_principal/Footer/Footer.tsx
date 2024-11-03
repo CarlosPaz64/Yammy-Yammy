71f8f1433dd1e3a23680e890b83159d66d037210
JoseCob <josecobkantun123@gmail.com>
2024-11-03 03:17:25 -0600

    Footer responsivo terminado
    Se termino el diseño del Footer de la página, ya se le agrego su lado responsivo y se hicieron algunos arreglos al diseño en la página en general.

--- src/pagina_principal/Footer/Footer.tsx
+++ src/pagina_principal/Footer/Footer.tsx
@@ -7,18 +7,39 @@ const Footer: React.FC<{ children?: ReactNode }> = ({ children }) => {
         <>
             {/*Pie de la página*/}
             <footer>
-                {/*Vista para pantallas grandes -> Footer estilo pastel*/}
+                {/*Footer estilo pastel*/}
                 <div className="bread-zone"></div>
-                <div className="container-chocolate">
-                    <div className="melted-chocolate1"></div>
-                    <div className="melted-chocolate2"></div>
-                    <div className="melted-chocolate3"></div>
-                    <div className="melted-chocolate4"></div>
-                    <div className="melted-chocolate5"></div>
-                    <div className="melted-chocolate6"></div>
-                    <div className="melted-chocolate7"></div>
-                    <div className="melted-chocolate8"></div>
-                    <div className="melted-chocolate9"></div>
+                {/*Vista para pantallas grandes -> chocolate derretido*/}
+                <div className="main-chocolate">
+                    <div className="container-chocolate">
+                        <div className="melted-chocolate1"></div>
+                        <div className="melted-chocolate2"></div>
+                        <div className="melted-chocolate3"></div>
+                        <div className="melted-chocolate4"></div>
+                        <div className="melted-chocolate5"></div>
+                        <div className="melted-chocolate6"></div>
+                        <div className="melted-chocolate7"></div>
+                        <div className="melted-chocolate8"></div>
+                        <div className="melted-chocolate9"></div>
+                    </div>
+                </div>
+                {/*Vista para tabletas -> chocolate derretido*/}
+                <div className="secondary-chocolate">
+                    <div className="container-chocolate">
+                        <div className="melted-chocolate1"></div>
+                        <div className="melted-chocolate2"></div>
+                        <div className="melted-chocolate3"></div>
+                        <div className="melted-chocolate4"></div>
+                        <div className="melted-chocolate5"></div>
+                    </div>
+                </div>
+                {/*Vista para celulares -> chocolate derretido*/}
+                <div className="tertiary-chocolate">
+                    <div className="container-chocolate">
+                        <div className="melted-chocolate1"></div>
+                        <div className="melted-chocolate2"></div>
+                        <div className="melted-chocolate3"></div>
+                    </div>
                 </div>
                 <div className="container-candle">
                     <div className="word-candle">
@@ -54,26 +75,26 @@ const Footer: React.FC<{ children?: ReactNode }> = ({ children }) => {
                         <i className="fi fi-ss-flame candle-flame flame14"></i>
                     </div>
                 </div>
-                <div className="part-pink"></div>
+                <div className="cake-layer"></div>
                 <section className="section-social">
                     {/*Redes sociales de la Repostería*/}
                     <h2 className="title-social">Siguenos</h2>
                     <div className="container-social">
-                        <a href="https://www.facebook.com/profile.php?id=100090051422438" target="_blank">
+                        <a href="https://www.facebook.com/profile.php?id=100090051422438" target="_blank" rel="noopener" aria-label="Síguenos en Facebook">
                             <img src="/src/assets/img-social/Simple_Icons/facebook.svg" alt="Facebook" className="social-icons" />
                         </a>
-                        <a href="https://www.instagram.com/yamy_postres?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">
+                        <a href="https://www.instagram.com/yamy_postres?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener" aria-label="Síguenos en Instagram">
                             <img src="/src/assets/img-social/Simple_Icons/instagram.svg" alt="Instagram" className="social-icons" />
                         </a>
-                        <a href="https://www.tiktok.com/@yamyyamyp?is_from_webapp=1&sender_device=pc" target="_blank">
+                        <a href="https://www.tiktok.com/@yamyyamyp?is_from_webapp=1&sender_device=pc" target="_blank" rel="noopener" aria-label="Síguenos en Tiktok">
                             <img src="/src/assets/img-social/Simple_Icons/tiktok.svg" alt="Tiktok" className="social-icons" />
                         </a>
-                        <a href="mailto:yamygomez.19@gmail.comsubject=Obtener más información" target="_blank">
+                        <a href="mailto:yamygomez.19@gmail.comsubject=Obtener más información" target="_blank" rel="noopener" aria-label="Síguenos en Gmail">
                             <img src="/src/assets/img-social/Simple_Icons/gmail.svg" alt="Gmail" className="social-icons" />
                         </a>
                     </div>
                 </section>
-                <div className="part-pink"></div>
+                <div className="cake-layer"></div>
                 <section className="section-slogan">
                     {/*Slogan con Derechos reservados */}
                     <div>
