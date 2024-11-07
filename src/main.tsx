5ed42797c94d5dc9d797095ebce40188d770eeb2
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-11-06 18:33:14 -0600

    Implementación de un Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; // Importa el Provider
+import { store } from './redux/store'; // Importa tu store
 import App from './App';
 import './pagina_principal/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}> {/* Proveedor de Redux */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
